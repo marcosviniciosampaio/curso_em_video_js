87c33edbad4c2edfb15491761a5174c823c48f78
marcos vinicio sampaio de araujo filho <marcosviniciosampaioaraujo@gmail.com>
2024-06-14 16:07:53 -0300

    array

---
+++ AULA13/ambiente2.js
@@ -0,0 +1,10 @@
+/*var c=1
+while(c<=10){
+    console.log('Hello World')
+    c++
+}*/
+console.log('vai comecar...')
+for(var c=1; c<=4; c++){
+    console.log(c)
+}
+console.log('fim')
---
+++ AULA15/ambiente3.js
@@ -0,0 +1,9 @@
+let num = [5,8,2,9,3]
+num[5] = 6
+num.push(7)
+num.length
+num.sort()
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posicoes`)
+console.log(`O primeiro valor é ${num[0]}`)
