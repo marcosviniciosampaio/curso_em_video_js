b2c89b79be1174c0d5c6afcb98478d36e52a5838
marcos vinicio sampaio de araujo filho <marcosviniciosampaioaraujo@gmail.com>
2024-06-14 10:43:35 -0300

    atualizacao

---
+++ AULA11/ex011.js
@@ -0,0 +1,8 @@
+var idade = 13
+if(idade <16){
+    console.log('Não vota')
+} else if (idade<18 || idade > 67 ){  
+    console.log('voto opcional')
+} else {
+    console.log('voto obrigatorio')
+}
